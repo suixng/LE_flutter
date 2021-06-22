c335c7880dbf630da6d1a7e8cea9382cb9f19802
suixng <suixingjun@gmail.com>
2021-06-22 11:37:29 +0800

    cradword

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,59 @@ void main() {
   runApp(MyApp());
 }
 
+class WordWidget extends StatefulWidget {
+  @override
+  _WordWidgetState createState() => _WordWidgetState();
+}
+
+class _WordWidgetState extends State<WordWidget> {
+  bool _isPressed = false;
+  String _word = 'english';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'English Learning',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('English'),
+        ),
+        // body: Center(
+        //   child: Text('English'),
+        // ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {},
+            child: Card(
+              color: Colors.blueAccent,
+              //z轴的高度，设置Card的阴影
+              elevation: 20.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              clipBehavior: Clip.antiAlias,
+              semanticContainer: false,
+              child: getChild(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  getChild() {
+    return Container(
+      color: Colors.deepPurpleAccent,
+      width: 300,
+      height: 250,
+      alignment: Alignment.center,
+      child: Text(
+        '$_word',
+        style: TextStyle(fontSize: 28, color: Colors.white),
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,11 +70,30 @@ class MyApp extends StatelessWidget {
         //   child: Text('English'),
         // ),
         body: Center(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset("images/pic6-1.jpg"),
+          child: Card(
+            color: Colors.blueAccent,
+            //z轴的高度，设置Card的阴影
+            elevation: 20.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            clipBehavior: Clip.antiAlias,
+            semanticContainer: false,
+            child: getChild(),
           ),
         ),
+      ),
+    );
+  }
+
+  getChild() {
+    return Container(
+      color: Colors.deepPurpleAccent,
+      width: 300,
+      height: 250,
+      alignment: Alignment.center,
+      child: Text(
+        'English',
+        style: TextStyle(fontSize: 28, color: Colors.white),
       ),
     );
   }
